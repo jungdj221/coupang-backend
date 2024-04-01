aa25351cef0e55560678e449176937fb0a347d77
jungdj221 <152463270+jungdj221@users.noreply.github.com>
2024-04-01 09:18:47 +0900

    Initial commit

---
+++ src/main/java/com/kh/coupang/CoupangBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.coupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangBackendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/coupang/CoupangBackendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.coupang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangBackendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
